88a7f9801efb4d80addf994541d46cc1bc9b20cd
ryan <1184518756@qq.com>
2019-10-29 17:45:50 +0800

    export more

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import {Mapping} from "./decorators/Mapping";
-import {Register} from "./decorators/Register";
-import {launcher} from "./launcher";
+import {Mapping, MappingDescribe} from "./decorators/Mapping";
+import {Register, RegisterDescribe} from "./decorators/Register";
+import {launcher, app} from "./launcher";
 
 exports.Mapping = Mapping;
 exports.Register = Register;
 exports.launcher = launcher;
+exports.MappingDescribe = MappingDescribe;
+exports.RegisterDescribe = RegisterDescribe;
+exports.app = app;
